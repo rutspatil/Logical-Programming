f18c8b5fc77b5f84841608810e9e73ba20a17c71
rutuja patil <rutujapatil991@gmail.com>
2022-03-06 15:29:43 +0530

    Adding first program

---
+++ LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class FibonacciSeries
+    {
+        public void fibonacci()
+        {
+            //input from user
+            Console.WriteLine("Enter the limit : ");
+            int lim = Convert.ToInt32(Console.ReadLine());  //converting from string to integer
+
+            if (lim > 0)
+            {
+                //Variable declaration
+                int y = 0;
+                int z = 1;
+                int s;
+                //Displaying starting two points
+                Console.WriteLine("Fibonacci series :  " + y + " " + z + " ");
+                //perform iterations till limit reaches
+                while (z <= lim)
+                {
+                    s = y + z;
+                    y = z;
+                    z = s;
+                    //condition for forcing z that it should not be printed when its value is greater than limit
+                    if (z <= lim)
+                        Console.WriteLine(z + "  ");
+                }
+            }
+
+            else
+                Console.WriteLine("Wrong Input");
+        }
+    }
+}
---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Program Number : ");
+            Console.WriteLine();
+            Console.WriteLine("1 = Fibonacci Number Series \n2 = Perfect Number \n3 = Prime Number \n4 = Reverse Number \n5 = StopWatch");
+            Console.Write("\nProgram number : ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    FibonacciSeries series = new FibonacciSeries();
+                    series.fibonacci();
+                    break;
+        }   }
+    }
+}
